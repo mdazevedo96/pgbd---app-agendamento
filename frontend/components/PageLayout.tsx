b6b1b9eed4f59faf8bda079f2a4b4f7456c37a58
BrunoAlvesTavares <brunoalvestavares@hotmail.com>
2025-11-03 17:56:26 -0300

    feat: conditionally render header based on current pathname

--- frontend/components/PageLayout.tsx
+++ frontend/components/PageLayout.tsx
@@ -2,18 +2,22 @@
 
 import React from "react";
 import Header from "./Header";
+import { usePathname } from "next/navigation";
 
 type PageLayoutProps = {
   children: React.ReactNode;
 };
 
 export default function PageLayout({ children }: PageLayoutProps) {
+  const pathname = usePathname();
+
+  const shouldRenderHeader = pathname !== "/login";
+
   return (
     <main className="min-h-screen bg-gray-50">
-      <Header />
+      {shouldRenderHeader && <Header />}
 
-      {/* Conteúdo principal */}
-      <section className="pt-30 px-4"> {/* <— aqui: pt-28 = 7rem */}
+      <section className="pt-30 px-4">
         {children}
       </section>
     </main>
